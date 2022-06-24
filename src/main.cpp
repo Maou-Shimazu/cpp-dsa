6a8d215731790fc6f9980030aa769856b197b6b3
Maou-Shimazu <eldaddanladi@gmail.com>
2022-06-24 02:21:36 -0600

    basic algorithms

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <iostream>
 #include "../include/cpp-dsa.hpp"
+#include <boost/algorithm/string.hpp>
 
-TEST_CASE("cpp-dsa") {
-    CHECK(add(1, 2) == 3);
+TEST_CASE("basic") {
+    CHECK(basic::add(1, 2) == 3);
+    CHECK(basic::largest(456, 67, 234) == 456);
+    CHECK(basic::quadratic(1, 2, 3) == "-1+j(1.4142135623730951) and -1-j(1.4142135623730951)");
+    CHECK(basic::factorial(5) == 120);
+    CHECK(basic::is_prime(2) == true);
+    CHECK(boost::trim_copy(std::string(basic::fibonacci(5))) == "1 2 3 5");
 }
