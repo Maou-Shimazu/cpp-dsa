010e68bef7ea75764a53a4b3b8091fb838eb2335
Eldad Danladi <eldaddanladi@gmail.com>
2022-08-17 07:07:42 -0600

    Que implimentation

--- include/cpp_dsa.hpp
+++ include/cpp_dsa.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
+#include <array>
+#include <vector>
 #ifndef CPP_DSA_HPP
 #define CPP_DSA_HPP
 
 #include <boost/algorithm/string.hpp>
 #include <cmath>
 #include <cstdint>
-#include <cstdlib> //mmmmmmmmmmm
 #include <doctest/doctest.h>
 #include <fmt/core.h>
 #include <fmt/format.h>
@@ -14,7 +15,8 @@
 #include <iostream>
 #include <src/format.cc>
 #include <sstream>
-#include <utility>
+
+constexpr uint64_t N_ONE = static_cast<uint64_t>(-1);
 
 namespace basic {
 	// Algo1
@@ -90,51 +92,61 @@ namespace Stack {
 		uint64_t top;
 
 	public:
-		Stack() : top(std::move(static_cast<uint64_t>(-1))){};
+		Stack() : top(std::move(N_ONE)){};
 		std::function<bool()> is_full = [this] {
-			return this->top == this->SMAX - 1 ? 1 : 0;
+			return top == SMAX - 1 ? 1 : 0;
 		};
-		std::function<bool()> is_empty = [this] {
-			return this->top == -1 ? 1 : 0;
-		};
+		std::function<bool()> is_empty = [this] { return top == -1 ? 1 : 0; };
 
 		void push(int new_item) {
-			if (this->is_full())
+			if (is_full())
 				std::cout << "STACK IS FULL";
 			else {
-				this->top++;
-				this->items[this->top] = new_item;
+				top++;
+				items[top] = new_item;
 			}
-			this->size++;
+			size++;
+		}
+
+		void push(std::vector<int> new_items) {
+			if (is_full())
+				std::cout << "Stack is full";
+			else {
+				for (auto& i : new_items) {
+					top++;
+					items[top] = i;
+				}
+			}
+			size += new_items.size();
 		}
 
 		void pop() {
-			if (this->is_empty())
+			if (is_empty())
 				std::cout << "\n STACK IS EMPTY \n";
 			else {
-				std::cout << "Item Popped = " << this->items[this->top] << "\n";
-				this->top--;
+				std::cout << "Item Popped = " << items[top] << "\n";
+				top--;
 			}
-			this->size--;
+			size--;
 		}
 
 		void printStack() {
 			std::cout << "Stack: ";
-			for (uint64_t i = 0; i < this->size; i++) {
-				std::cout << this->items[i] << " ";
+			for (uint64_t i = 0; i < size; i++) {
+				std::cout << items[i] << " ";
 			}
 			std::cout << "\n";
 		}
 	};
 
-	int _main() {
+	int main() {
 		std::cout << "Stack Output: " << std::endl;
 		Stack s = Stack();
-
-		s.push(1);
-		s.push(2);
-		s.push(3);
-		s.push(4);
+		// s.push(1);
+		// s.push(2);
+		// s.push(3);
+		// s.push(4);
+		s.push({1, 2, 3, 4});
 		s.printStack();
 		s.pop();
 		std::cout << "\nAfter popping out\n";
@@ -143,5 +155,89 @@ namespace Stack {
 		return 0;
 	}
 } // namespace Stack
+
+namespace Queue { // warning: output is different from expected.
+	class Queue {
+		static constexpr int SIZE = 5;
+		std::array<int, SIZE> items;
+		uint64_t front, rear;
+
+	public:
+		Queue() : front(std::move(N_ONE)), rear(std::move(N_ONE)) {}
+		bool is_full() {
+			return front == 0 && rear == SIZE - 1 ? true : false;
+		}
+		bool is_empty() {
+			return front == -1 ? true : false;
+		}
+		void en_queue(int element) {
+			if (is_full())
+				std::cout << "Queue is full\n";
+			else {
+				front == 1 ? front = 0 : rear++;
+				items[rear] = element;
+				std::cout << "Inserted = " << element << "\n";
+			}
+		}
+		void en_queue(std::vector<int> elements) {
+			if (is_full())
+				std::cout << "Queue is full\n";
+			else {
+				for (auto& i : elements) {
+					front == 1 ? front = 0 : rear++;
+					items[rear] = i;
+					std::cout << "Inserted = " << i << "\n";
+				}
+			}
+		}
+		int de_queue() {
+			if (is_empty()) {
+				std::cout << "Queue is Empty\n";
+				return -1;
+			} else {
+				int element = items[front];
+				if (front >= rear) {
+					front = N_ONE;
+					rear  = N_ONE;
+				} else
+					front++;
+				std::cout << "Deleted = " << element << "\n";
+				return element;
+			}
+		}
+		void display() {
+			if (is_empty())
+				std::cout << "Empty Queue\n";
+			else {
+				std::cout << "Front index: " << front;
+				std::cout << "Items: ";
+				for (auto i = front; i <= rear; i++)
+					std::cout << items[i] << " ";
+				std::cout << "Rear Index: " << rear << "\n";
+			}
+		}
+	};
+	int main() {
+		std::cout << "Queue: \n";
+		Queue q = Queue();
+
+		q.de_queue();
+
+		q.en_queue(1);
+		q.en_queue(2);
+		q.en_queue(3);
+		q.en_queue(4);
+		q.en_queue(5);
+
+		q.en_queue(6);
+		q.display();
+
+		q.de_queue();
+		q.display();
+
+		std::cout << "\n";
+		return 0;
+	}
+} // namespace Queue
 
 #endif
